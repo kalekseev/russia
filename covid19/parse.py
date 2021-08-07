0753e5bad025c7325e884958cc5af2eeb945f880
Konstantin Alekseev <mail@kalekseev.com>
2021-08-08 01:12:29 +0300

    may+june 2021

--- covid19/parse.py
+++ covid19/parse.py
@@ -63,7 +63,7 @@ def with_covid_read_month_deaths(year: int, month: int) -> dict:
     try:
         data = pd.read_excel(
             DATA_DIR / f"rosstat/covid/{year}/{month}.xlsx",
-            usecols="A,B,E",
+            usecols="A,B,K" if (year > 2020 and month > 4) else "A,B,E",
             skiprows=9,
             index_col=0,
             header=None,
@@ -75,7 +75,7 @@ def with_covid_read_month_deaths(year: int, month: int) -> dict:
         data = data[:-1]
     data = data.to_dict()
     from_covid = data[1]
-    with_covid = data[4]
+    with_covid = data[10 if (year > 2020 and month > 4) else 4]
     data = {
         region: (
             0 if pd.isna(v) else int(v),
