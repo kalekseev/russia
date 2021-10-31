3adb63f550175481833b3fb0e8401d7f8d928c7d
Konstantin Alekseev <mail@kalekseev.com>
2021-11-01 00:21:11 +0300

    tooling

--- covid19/shell.nix
+++ covid19/shell.nix
@@ -1,12 +1,16 @@
-{ pkgs ? import <nixpkgs> { }, system ? builtins.currentSystem }:
-with pkgs;
+{ system ? builtins.currentSystem }:
 let
   src = fetchTarball {
     url = "https://github.com/numtide/devshell/archive/f64db97388dda7c2c6f8fb7aa5d6d08365fb1e01.tar.gz";
     sha256 = "1421h6bhsg4fishz10092m71qnd5ll6129l45kychzh9kp23040s";
   };
+  pkgsSrc = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/3a2e0c36e79cecaf196cbea23e75e74710140ea4.tar.gz";
+    sha256 = "0gjv2y6vjn3sdpg8ljcw2mk99c1xxdrfv11pc8kf5ms64wby20g5";
+  };
+  pkgs = import pkgsSrc { };
   devshell = import src { inherit system; };
-  python = python3.withPackages (ps: [ ps.pandas ps.xlrd ps.ipython ]);
+  python = pkgs.python3.withPackages (ps: [ ps.pandas ps.xlrd ps.ipython ]);
 in
 devshell.mkShell {
   name = "russia";
